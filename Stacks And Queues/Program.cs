0521922697fcafcae9a13c7d26d9ee087f4b8871
poonambudde <buddepoonam123@gmail.com>
2022-02-25 21:17:59 +0530

    [Poonam] ADDED: Welcome to Stacks and Queues Programs

---
+++ Stacks And Queues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stacks_And_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Programs");
+        }
+    }
+}
