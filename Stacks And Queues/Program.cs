c0fc387d594e0b9dfe71bf0966ead03c150d89e1
poonambudde <buddepoonam123@gmail.com>
2022-02-27 07:11:20 +0530

    [Poonam] ADDED: Ability to create a Stack

--- Stacks And Queues/Program.cs
+++ Stacks And Queues/Program.cs
@@ -7,6 +7,13 @@ namespace Stacks_And_Queues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues Programs");
+
+            StackUsingLinkList stackUsingLinkList = new StackUsingLinkList();
+            stackUsingLinkList.push(70);
+            stackUsingLinkList.push(30);
+            stackUsingLinkList.push(56);
+            stackUsingLinkList.Display();
+
         }
     }
 }
