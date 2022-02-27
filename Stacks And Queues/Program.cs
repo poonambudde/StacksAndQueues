11e5024d72cb8ae507c95bf558cbc0c4a980a24f
poonambudde <buddepoonam123@gmail.com>
2022-02-27 08:20:20 +0530

    [Poonam] ADDED: Ability to peak and pop from the Stack till it is empty

--- Stacks And Queues/Program.cs
+++ Stacks And Queues/Program.cs
@@ -9,11 +9,17 @@ namespace Stacks_And_Queues
             Console.WriteLine("Welcome to Stacks and Queues Programs");
 
             StackUsingLinkList stackUsingLinkList = new StackUsingLinkList();
-            stackUsingLinkList.push(70);
-            stackUsingLinkList.push(30);
-            stackUsingLinkList.push(56);
+            stackUsingLinkList.Push(70);
+            stackUsingLinkList.Push(30);
+            stackUsingLinkList.Push(56);
             stackUsingLinkList.Display();
 
+            stackUsingLinkList.Peak();
+            stackUsingLinkList.Display();
+            stackUsingLinkList.POP();
+            stackUsingLinkList.Display();
+            stackUsingLinkList.IsEmpty();
+            stackUsingLinkList.Display();
         }
     }
 }
